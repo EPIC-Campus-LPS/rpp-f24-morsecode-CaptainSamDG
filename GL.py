4115666d9006a2fe2e400405ce2741aaaa06eaa5
CaptainSamDG <166750443+CaptainSamDG@users.noreply.github.com>
2024-11-13 11:54:05 -0700

    Add files via upload
    This plays the General Lee horn when ever the user presses the button.

---
+++ GL.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+from pydub import AudioSegment
+from pydub.playback import play
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+button = GPIO.setup(25,GPIO.IN) #green
+
+x = 0
+while x == 0:
+	if not GPIO.input(25):
+
+		sound =  AudioSegment.from_file("general-lee-horn.mp3", format="mp3")
+		play(sound)
+		x += 1
